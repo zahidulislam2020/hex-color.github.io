7013c3a03c5ad4228e95d9936ea55768084a1f9f
Zahidul Islam <61370815+zahidulislam2020@users.noreply.github.com>
2020-11-18 07:45:52 +0300

    optimized script.js

--- js/script.js
+++ js/script.js
@@ -5,25 +5,22 @@ let button = document.querySelector('.button');
 let hexNumAndAlph = [0, 1, 2, 3, 4, 5, 6, 6, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 
 button.addEventListener('click', () => {
-	let randValueOfArray = randomValue();
-	let randHexColorCode = '';
-	
-	randValueOfArray.map((value) => {
-  	randHexColorCode += value;
- })
+	let randHexColorCode = randomValue();
 
 	body.style.background = `#${randHexColorCode}`;
  	displayCodeField.innerHTML = `Hex Color Code: #${randHexColorCode}`;
-
+  
 })
 
 let randomValue = () => {
-	
-let newArray = []
+
+	let randHexColorCode = '';
 	
   for(let i = 1; i <= 6; i++) {
   	var indexOfArray = Math.floor(Math.random() * hexNumAndAlph.length);
-    newArray.push(hexNumAndAlph[indexOfArray])
+    
+    randHexColorCode += hexNumAndAlph[indexOfArray];
   }
-  return newArray;
+  
+  return randHexColorCode
 }
