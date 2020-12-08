38e0b4cf68ccf42000d0b742cbf172054cc9b902
Zahidul Islam <61370815+zahidulislam2020@users.noreply.github.com>
2020-12-08 12:58:51 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 let body = document.querySelector('body');
 let displayCodeField = document.querySelector('.code-display');
 let button = document.querySelector('.button');
+let autoBtn = document.querySelector('.manual-auto');
+let currentDelayTime = document.querySelector('.current-delay-time');
+let timeInput = document.querySelector('.delay-time');
+
+let stopId;
+let blinkStop;
+
+
+
+let timeSet = () => captureInputValue();
+
+function captureInputValue() {
+
+	let timeValue = Number(timeInput.value);
+
+	if(timeValue > 60) {
+		timeInput.value = '60';
+		timeValue = 60;
+		return timeValue
+	}else {
+		if(timeValue < 1) {
+			timeInput.value = '1';
+			timeValue = 1;
+			return timeValue;
+		}else {
+			return timeValue
+		}
+	}
+}
+
 
 let hexNumAndAlph = [0, 1, 2, 3, 4, 5, 6, 6, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 
@@ -22,5 +52,48 @@ let randomValue = () => {
     randHexColorCode += hexNumAndAlph[indexOfArray];
   }
   
-  return randHexColorCode
+  return randHexColorCode;
+};
+
+autoBtn.addEventListener('click', actionToggle);
+
+let isAuto = false;
+
+function actionToggle() {
+	if(!isAuto) {
+		autoBtn.innerHTML = 'Set to Manual';
+		timeInput.removeAttribute('disabled', '');
+		currentDelayTime.removeAttribute('disabled', '');
+		button.setAttribute('disabled', '');
+		blinkStop = setInterval(blink, 500)
+		setTimeout(runAuto =() => {
+	
+			let randHexColorCode = randomValue();
+
+			body.style.background = `#${randHexColorCode}`;
+			displayCodeField.innerHTML = `Hex Color Code: #${randHexColorCode}`;
+			
+			stopId = setTimeout(runAuto, 1000 * timeSet());
+					
+			blinking.innerHTML = timeSet();		
+
+}, 1000 * timeSet())
+		
+		isAuto = true;
+	}else {
+		autoBtn.innerHTML = 'Set to Auto';
+		timeInput.setAttribute('disabled', '');
+		currentDelayTime.setAttribute('disabled', '');
+		button.removeAttribute('disabled', '');
+		clearTimeout(blinkStop);
+		clearTimeout(stopId);
+		isAuto = false;
+	}
 }
+
+
+let blinking = document.querySelector('.a');
+	
+	function blink() {
+		blinking.classList.toggle('blink')
+	}
